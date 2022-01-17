099cec1448f12d235432bfaa01ade9f52e133202
Dimitar Radulov <dimitaradulov@gmail.com>
2022-01-17 20:38:56 +0200

    Added slider moving on left or right arrow click

--- script.js
+++ script.js
@@ -1,26 +1,56 @@
 'use script';
 const slider = document.querySelector('.slider');
-const sliderBtnRight = document.querySelectorAll('.slider__btn--right');
-const sliderBtnLeft = document.querySelectorAll('.slider__btn--left');
+const sliderBtnRight = document.querySelector('.slider__btn--right');
+const sliderBtnLeft = document.querySelector('.slider__btn--left');
 let slides = [];
 
-fetch(
-  'https://api.unsplash.com/topics/nature/photos?client_id=d3oyf0zyFNLdAUaurrFrBFdnifqvPzFQnMicBrbclBU'
-)
-  .then((res) => res.json())
-  .then((res) => {
-    res.forEach((photo, i) => {
-      const slide = document.createElement('div');
-      slide.classList.add('slide');
+const fetchCall = async () => {
+  const result = await fetch(
+    'https://api.unsplash.com/topics/nature/photos?client_id=d3oyf0zyFNLdAUaurrFrBFdnifqvPzFQnMicBrbclBU'
+  );
+  const jsonRes = await result.json();
 
-      slide.insertAdjacentHTML(
-        'afterbegin',
-        `<img src='${photo.urls.regular}' alt='Nature Photo ${i}' />`
-      );
+  jsonRes.forEach((photo, i) => {
+    const slide = document.createElement('div');
+    slide.classList.add('slide');
 
-      slider.prepend(slide);
+    slide.insertAdjacentHTML(
+      'afterbegin',
+      `<img src='${photo.urls.regular}' alt='Nature Photo ${i}' />`
+    );
 
-      slides.push(slide);
+    slider.prepend(slide);
+
+    slides.push(slide);
+  });
+
+  let currSlide = 0;
+  const maxSlides = slides.length - 1;
+
+  const goToSlide = (slide) => {
+    slides.forEach((p, i) => {
+      console.log(i);
+      p.style.transform = `translateX(${100 * (i - slide)}%)`;
     });
-  })
-  .catch((err) => console.error(err));
+  };
+
+  goToSlide(0);
+
+  const nextSlide = () => {
+    if (currSlide === maxSlides) currSlide = 0;
+    else currSlide++;
+    goToSlide(currSlide);
+  };
+
+  const prevSlide = () => {
+    if (currSlide === 0) currSlide = maxSlides;
+    else currSlide--;
+    goToSlide(currSlide);
+  };
+
+  sliderBtnRight.addEventListener('click', nextSlide);
+
+  sliderBtnLeft.addEventListener('click', prevSlide);
+};
+
+fetchCall();
