7a6d6e68f2838d0216c2a59596f3f476ffeff1d4
Dimitar Radulov <dimitaradulov@gmail.com>
2022-01-17 19:08:21 +0200

    Added slider style and buttons

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use script';
+
+// fetch(
+//   'https://api.unsplash.com/topics/nature/photos?client_id=d3oyf0zyFNLdAUaurrFrBFdnifqvPzFQnMicBrbclBU'
+// )
+//   .then((res) => res.json())
+//   .then((res) => {
+//     body.insertAdjacentHTML(
+//       'afterbegin',
+//       `<img src='${res[0].urls.regular}' />`
+//     );
+//   })
+//   .catch((err) => console.error(err));
