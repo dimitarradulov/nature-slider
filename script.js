f6aed2727d575b6b6eb79e3f1b9d625c9b4e8f8f
Dimitar Radulov <dimitaradulov@gmail.com>
2022-01-17 19:30:36 +0200

    Add fetching imgs from unsplash api

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 'use script';
+const slider = document.querySelector('.slider');
+const sliderBtnRight = document.querySelectorAll('.slider__btn--right');
+const sliderBtnLeft = document.querySelectorAll('.slider__btn--left');
+let slides = [];
 
-// fetch(
-//   'https://api.unsplash.com/topics/nature/photos?client_id=d3oyf0zyFNLdAUaurrFrBFdnifqvPzFQnMicBrbclBU'
-// )
-//   .then((res) => res.json())
-//   .then((res) => {
-//     body.insertAdjacentHTML(
-//       'afterbegin',
-//       `<img src='${res[0].urls.regular}' />`
-//     );
-//   })
-//   .catch((err) => console.error(err));
+fetch(
+  'https://api.unsplash.com/topics/nature/photos?client_id=d3oyf0zyFNLdAUaurrFrBFdnifqvPzFQnMicBrbclBU'
+)
+  .then((res) => res.json())
+  .then((res) => {
+    res.forEach((photo, i) => {
+      const slide = document.createElement('div');
+      slide.classList.add('slide');
+
+      slide.insertAdjacentHTML(
+        'afterbegin',
+        `<img src='${photo.urls.regular}' alt='Nature Photo ${i}' />`
+      );
+
+      slider.prepend(slide);
+
+      slides.push(slide);
+    });
+  })
+  .catch((err) => console.error(err));
